7f4f58ad41845fdb3a34919d6081453e4b631f0b
rakhimishra <mishrarakhi21@gmail.com>
2020-09-16 20:20:13 +0530

    changes

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class Clock extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            time1: "",
+            flag: true
+        }
+    }
+    
     formatTime(timeInSeconds) {
         var seconds = timeInSeconds % 60;
         var minutes = Math.floor(timeInSeconds / 60);
@@ -15,14 +23,59 @@ class Clock extends React.Component {
 
         return minutes + ':' + seconds;
     }
+    // interval(time2){
+        
+    //     setInterval(() => {
+    //         time2-=1;
+    //         console.log(this.formatTime(time2));
+    //         this.setState( {time1:"5"})
+          
+    //     }, 1000);
+
+    // }
+    myfunc=(seconds)=>{
+        // const temp = this.formatTime(seconds)
+        // console.log(temp)
+        console.log(seconds)
+        var temp2 = seconds
+                var time  = setInterval(()=>{
+                console.log("checking")
+                temp2-=1;
+                this.setState({
+                    time1:temp2
+                })
+                
+                if (temp2 ===0){
+                    clearInterval(time)
+                }
+                },1000);
+        
+              
+
+    
+}
+      
+   
+
 
     render() {
         var {timeInSeconds} = this.props;
+
+        if (this.state.flag===true){
+            // myfunc()
+        } 
+        
+        
+        
+        // this.setState({time1:time2});
+        
+        
+        
         //Keep the classes name. Try to inject your code and do not remove existing code
         return (
             <div className="clock">
                 <span className="clock-text">
-                  
+                    {this.state.time1}
                 </span>
             </div>
         );
--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import Clock from './Clock';
-import CountdownForm from './CountdownForm';
 
 class Countdown extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            count: null
+
+        }
+        // this.childRef = React.createRef()
+    }
+
+
+    onSubmit(e) {
+        e.preventDefault()
+        //This will give you string for seconds. Do not remove refs
+         var secondsStr = this.refs.seconds.value;
+        this.setState({count:secondsStr});
+        this.refs.seconds.value=" "
+        this.refs.childRef.myfunc(secondsStr)
+ 
+        
+     }
   
 
     render() {
-
+        
         return (
             <div>
-                <Clock timeInSeconds={count}/>
-                <CountdownForm onSetCountdownTime={}/>
+                <Clock timeInSeconds={this.state.count} ref="childRef"/>
+                <div>
+                <form ref="form" onSubmit={this.onSubmit.bind(this)} className="countdown-form">
+                    <input type="text" ref="seconds" placeholder="Enter time in seconds"/>
+                    <input type="submit" className="button success expanded" value="Start Countdown"/>
+                </form>
+            </div>
             </div>
         );
     }
